552f249fe98c14d9362311f7aa943cb1e1de4b02
Rohit Kumar <147086495+rohit-codes7@users.noreply.github.com>
2025-12-01 05:15:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Index/find-index.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+class Solution {
+    // Function to find starting and end index
+    static int[] findIndex(int arr[], int key) {
+        // code here.
+        int start = -1;
+        int end =  -1;
+        
+        for(int i =0;i<arr.length;i++){
+            if(arr[i] == key){
+                if(start == -1) start = i;
+                end = i;
+            }
+        }
+        return new int[]{start,end};
+    }
+}
