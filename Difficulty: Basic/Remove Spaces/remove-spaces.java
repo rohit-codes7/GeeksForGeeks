04037e158a7e75c8522b034e1958958706170597
Rohit Kumar <147086495+rohit-codes7@users.noreply.github.com>
2025-12-12 21:19:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+
+    String modify(String s) {
+        // your code here
+        s = s.replace(" ", "");
+        
+        return s;
+    }
+}
